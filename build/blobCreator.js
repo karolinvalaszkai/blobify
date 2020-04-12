3fa5a14ff7be86d10849877b9aa3dc150b4ba7fd
Sabina Nordell <sabnor@kth.se>
2020-04-12 11:58:34 +0200

    Build & firebase for usability tests

---
+++ build/blobCreator.js
@@ -0,0 +1,202 @@
+
+//let blobCreator = window.blobCreator
+function blobCreator(songObj) {
+    for (prop in songObj){
+      if (prop == 0.0){
+        prop = 0.001;
+      }
+    }
+
+    //console.log("Two script running",songObj)
+    song_id = songObj.id;
+    //pitches = songObj.pitches;
+    energy = songObj.energy;
+    valence = songObj.valence;
+    //pitches.push.apply(pitches,pitches);
+    //pitches.push.apply(pitches,pitches);
+
+
+
+
+  //Creates the div where the blob will be placed
+    //var elem = document.getElementById(songObj.id)
+    var newDiv = document.createElement("div");
+
+    //elem.appendChild(newDiv);
+
+var two = new Two({
+    type: Two.Types.svg,
+    fullscreen: false,
+    width: 300, 
+    height: 300
+  }).appendTo(newDiv);
+
+  var mass = 100*Math.sqrt(energy);
+  var radius = two.height / 6;
+  //var strength = 0.0625;
+
+  //Sets speed of animation:
+  var strength = Math.pow(energy,2);
+  var drag = 0.0;
+
+  var background = two.makeGroup();
+  var foreground = two.makeGroup();
+
+  var physics = new Physics();
+  var points = [];
+  var i = 0;
+
+  //Sets number of points
+  Two.Resolution = Math.round(25*Math.sqrt(energy))+3;
+
+
+  for (i = 0; i < Two.Resolution; i++) {
+    //console.log(Two.Resolution)
+    var pct = i / Two.Resolution;
+    var theta = pct * Math.PI * 2;
+
+    var ax = radius * Math.cos(theta);
+    var ay = radius * Math.sin(theta);
+
+    var variance = Math.random() * (1-energy) + 0.6;
+    var bx = variance * ax;
+    var by = variance * ay;
+
+    var origin = physics.makeParticle(mass, ax, ay)
+    var particle = physics.makeParticle(Math.random() * mass * 0.66 + mass * 0.33, bx, by);
+    var spring = physics.makeSpring(particle, origin, strength, drag, 0);
+
+    origin.makeFixed();
+
+    particle.shape = two.makeCircle(particle.position.x, particle.position.y, 1);
+    particle.shape.noStroke().fill = '#000';
+    particle.position = particle.shape.translation;
+
+    foreground.add(particle.shape)
+    points.push(particle.position);
+
+  }
+
+  var outer = new Two.Path(points, true, true);
+  var color = getRandomColor();
+  outer.stroke = color.toString();
+  outer.fill = color.toString(0.5);
+  outer.scale = 1.75;
+  outer.linewidth = 1;
+
+  background.add(outer);
+
+  var inner = new Two.Path(points, true, true);
+  inner.noStroke();
+  inner.fill = getRandomColor().toString();
+  inner.scale = 1.25;
+
+  background.add(inner);
+
+  //two.renderer.domElement.style.background = 'url(' + generateGrid() + ') center center';
+
+  resize();
+
+  
+
+  two
+    .bind('resize', resize)
+    .bind('update', function() {
+      physics.update();
+    })
+    .play();
+    
+    var startMotion = true;
+    setInterval(function(){
+      if (startMotion === true){
+      two.bind('resize', resize).pause()
+      startMotion = false;
+      } 
+    },2000);
+
+    var audioElem = document.getElementById("audio"+songObj.id);
+    
+    $(window).keypress(function (e) {
+        if (e.key === ' ' || e.key === 'Spacebar') {
+          e.preventDefault()
+          console.log('Space pressed')
+          two.bind('resize', resize).play();
+        }
+      })
+      $(window).keypress(function (e) {
+        if (e.key === 'p' || e.key === 'P') {
+          e.preventDefault()
+          console.log('Space pressed')
+          two.bind('resize', resize).pause();
+        }
+      })
+
+ 
+  
+        $(two.renderer.domElement).mouseenter(function(){
+          if (startMotion === false){
+            two.bind('resize', resize).play();
+
+            //So it doesn't try to play the music without user interaction (which gives error)
+            if(audioElem.muted ===false)
+            {audioElem.play()}
+          }
+          
+          
+        })
+        $(two.renderer.domElement).mouseleave(function(){
+          audioElem.pause()
+          two.bind('resize', resize).pause();
+        })
+  
+
+
+  function resize() {
+    background.translation.set(two.width / 2, two.height / 2);
+    foreground.translation.copy(background.translation);
+  }
+
+  function getRandomColor() {
+    var blobEnergyColour = Math.pow(energy,1)
+    if (blobEnergyColour<0.70 && blobEnergyColour>0.65){
+      r= Math.floor(1 * 255)
+      g = Math.floor(1 * 255)
+      b = Math.floor((1-energy)* 255)
+
+    }
+
+    else if (blobEnergyColour<0.65 && blobEnergyColour>0.55){
+      r= Math.floor(0 * 255)
+      g = Math.floor(energy * 255)
+      b = Math.floor((1-energy)* 255)
+
+    }
+    else {
+      r = Math.floor(blobEnergyColour * 255)
+      g = Math.floor(0.3 * 255)
+      b = Math.floor((1-blobEnergyColour) * 255)
+    }
+
+    var color = {
+      r: r,
+      g: g,
+      b: b,
+      toString: function(a) {
+        if (a) {
+          return 'rgba('
+            + color.r + ','
+            + color.g + ','
+            + color.b + ','
+            + a + ')';
+        }
+        return 'rgb('
+          + color.r + ','
+          + color.g + ','
+          + color.b + ')';
+      }
+    };
+    return color;
+  }
+  return newDiv;
+
+}
---
+++ build/precache-manifest.0f2ac6572759c1e14a1be926460ceff8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967d9030e5629f2218ba40841e905879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a566b4dbf045d36c3dd6",
+    "url": "/static/css/main.1a74a09f.chunk.css"
+  },
+  {
+    "revision": "b888e06d4f73a39a2e65",
+    "url": "/static/js/2.aafbc390.chunk.js"
+  },
+  {
+    "revision": "a31dab8a13058e5d01524b9674f1b673",
+    "url": "/static/js/2.aafbc390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a566b4dbf045d36c3dd6",
+    "url": "/static/js/main.dcf6ad36.chunk.js"
+  },
+  {
+    "revision": "b5a2e31acc900960b118",
+    "url": "/static/js/runtime-main.02a028ec.js"
+  },
+  {
+    "revision": "9747c2216b2edf059481d6d212864734",
+    "url": "/static/media/Gilroy-Bold.9747c221.woff"
+  },
+  {
+    "revision": "0e2ee5593d6806aed16dd6edd3bdd673",
+    "url": "/static/media/Gilroy-Heavy.0e2ee559.woff"
+  },
+  {
+    "revision": "ef3d6f936e4825929a849b16abeac0ec",
+    "url": "/static/media/Gilroy-Light.ef3d6f93.woff"
+  },
+  {
+    "revision": "d35dfeb079581284803973c49177dfc6",
+    "url": "/static/media/Gilroy-Medium.d35dfeb0.woff"
+  },
+  {
+    "revision": "b963b73c1c9b881a10faf8b4d363b85b",
+    "url": "/static/media/Gilroy-Regular.b963b73c.woff"
+  },
+  {
+    "revision": "be4035b78e8b0833f535dae26f77a036",
+    "url": "/static/media/mute.be4035b7.svg"
+  },
+  {
+    "revision": "af77da1cb8130eee4b23efe6648a8839",
+    "url": "/static/media/sidebar.af77da1c.svg"
+  },
+  {
+    "revision": "c132a0c76a177fb3fa4ddecfc3f90eb5",
+    "url": "/static/media/unmute.c132a0c7.svg"
+  }
+]);
