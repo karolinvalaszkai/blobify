00d3edd1b960be70ce8c23449ba32df08c916af1
Karolin Valaszkai <karolinv@kth.se>
2020-04-20 17:36:55 +0200

    Removed about and blobify menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Navbar from './Navbar/NavbarContainer'
-import BlobifyMenu from './BlobifyMenu/BlobifyContainer'
-import About from './About/AboutContainer'
 import '../style.css'
 import Search from './Search/SearchContainer'
 import Playlist from './Playlist/PlaylistContainer'
@@ -17,8 +15,6 @@ const App = () => (
         <Route path="/trending" component={Search}></Route>
         <Route path="/myplaylist" component={Playlist}></Route>
         <Route component={Navbar}></Route>
-        {/* <Route component={BlobifyMenu}></Route>
-        <Route component={About}></Route> */}
       </div>
   </Router>
 )
