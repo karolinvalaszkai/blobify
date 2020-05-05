f11f07ea61e83c39cce498616d8f5fbd4db4bedb
Karolin Valaszkai <karolinv@kth.se>
2020-05-05 23:50:29 +0200

    Update css

--- src/components/Navbar/NavbarPresentational.js
+++ src/components/Navbar/NavbarPresentational.js
@@ -8,6 +8,7 @@ const NavbarPresentational = ({songs, muted, nav,  homepageControl, playlistCont
   const dragOverEvent = onDragOver;
 
 
+
   return (
     <div id="navbar" className="navbar debug nothidden">
       {/* <h1 id="menu-title" onClick={() => openNav(nav.navbarHidden)}>blobify</h1> */}
@@ -19,30 +20,31 @@ const NavbarPresentational = ({songs, muted, nav,  homepageControl, playlistCont
         <div className="navbarContent-playlist">
           {/* <button onClick={() => homepageAction()}>{homepageMessage}</button> */}
           <h2 id="content-title">Pick playlist</h2>
-            <h4 id="content-title">Switch playlist to <span className="tooltip" onClick={(e)=>openTooltip(e, 'info')} onContextMenu={(e)=>openTooltip(e, 'info')}>blobify songs <img className='question' src="info.svg"  alt="info" height='18' width='18'/>  
+            <h4 id="content-title">Switch playlist to blobify songs <span className="tooltip" onClick={(e)=>openTooltip(e, 'info')} onContextMenu={(e)=>openTooltip(e, 'info')}> <img className='question' src="info.svg"  alt="info" height='18' width='18'/>  
 
                 <div id={"tooltip-"+'info'} className="tooltiptext hidden">
+
+                {/* <div className="cross close-summary" onClick={(e)=>openTooltip(e, 'info')}></div> */}
+
                 <h3>What you see here is not random colors and shapes.</h3><br/>
 
                 <h4>A blob is a visual representation of a tracks <a className="link" href="https://developer.spotify.com/documentation/web-api/reference/tracks/get-several-audio-features/" target="_blank">audio features</a> - data on its energy, tempo and key.<br/><br/><br/>
-                {/* 
-                <div className="energy_explain">
-                  <h4>the<h6>energy</h6>of the track</h4>
-                </div> */}
-                {/* <h4> */}
-                The ENERGY of the track is mapped to the number of dots in the blobs which determines its shape.<br/><br/> 
+                
+                
+                {/* The <span className="h4_white">energy</span> of the track is mapped to the number of dots in the blobs which determines its <span className="h4_white">shape</span>.<br/><br/>  */}
+
+                The energy of the track is mapped to the number of dots in the blobs which determines its shape.<br/><br/> 
                 {/* <h4>The higher the energy, the more aggressive blob.</h4><br/> */}
 
-                The TEMPO of the track is mapped to the speed of the blob.<br/><br/>
+                The tempo of the track is mapped to the speed of the blob.<br/><br/>
 
                                                     
-                The KEY of the track is mapped to the color of the blob based on <a className="link" href="http://www.harmonics.com/lucy/lsd/colors.html" target="_blank">Charles Fourier's theory.</a><br/><br/><br/>
-
+                The key of the track is mapped to the color of the blob based on <a className="link" href="http://www.harmonics.com/lucy/lsd/colors.html" target="_blank">Charles Fourier's theory.</a><br/><br/><br/>
+                </h4>
+                
                 {/* - We call it to blobify songs. <br/><br/> */}
 
-                {/* </h4> */}
-                Click on the blobs for more.
-                </h4>
+                <h6>Click on the blobs for more.</h6>
                 </div>
               </span>
             </h4>
