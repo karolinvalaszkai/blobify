623b8ec28f9f2123cfc4e3869cb42cbd2c0dbf2a
Karolin Valaszkai <karolinv@kth.se>
2021-01-23 03:29:03 +0100

    Update NavbarPresentational.js

--- src/components/Navbar/NavbarPresentational.js
+++ src/components/Navbar/NavbarPresentational.js
@@ -17,17 +17,17 @@ const NavbarPresentational = ({songs, muted, nav,  homepageControl, playlistCont
     {/* <div id="mobile_pop_up"><span id="mobile_pop_up-content"><h2>Rotate your device 90°</h2><br/><h4 className="h4_white">psst... try force touch on the blobs</h4></span><div className="backgroundSummary"></div></div> */}
 
       <div className="navbarContent">
-        <h1 id="menu-title"><span href="/">blobify.</span></h1>
+        <h1 id="menu-title"><span href="/">blobify.</span></h1> <br/>
+      
         <div className="menu">
+
+
           <div className="navbarContent-playlist">
+
             {/* <h2 id="content-title">Pick Playlist</h2> */}
             <h2>What you see here is not random colors and shapes.</h2>
-              <h4>A blob is a visual representation of a song's <a className="link" href="https://developer.spotify.com/documentation/web-api/reference/tracks/get-several-audio-features/" target="_blank">audio features</a> - data on its energy, tempo and key.<br/></h4>
-              <h6>
-                The energy of the song is mapped to the number of dots in the blobs which determines its shape.<br/><br/>
-                The tempo of the song is mapped to the movement of the blob.<br/><br/>
-                The key of the song is mapped to the color of the blob based on <a className="grey_link" href="http://www.harmonics.com/lucy/lsd/colors.html" target="_blank">Charles Fourier's Theory.</a><br/>
-              </h6>
+              <h4>A blob is a data visualization  of a song's <a className="link" href="https://developer.spotify.com/documentation/web-api/reference/tracks/get-several-audio-features/" target="_blank">audio features</a> - energy, tempo and key.<br/></h4><br/>
+                {/* <img  src="blob.svg"  alt="blobyfied song" width='50%'/><br/><br/><br/><br/> */}               
               {/* <h4 id="content-title">Switch playlist to blobify songs <span className="tooltip"> <img className='question' src="info.svg"  alt="info" height='13' width='13' onClick={(e)=>openTooltip(e, 'info')} onContextMenu={(e)=>openTooltip(e, 'info')}/>
                 <div id={"tooltip-"+'info'} className="tooltiptext hidden">
                   <div className="tooltip-content">
@@ -47,12 +47,13 @@ const NavbarPresentational = ({songs, muted, nav,  homepageControl, playlistCont
               </span>
             </h4> */}
 
-            <div className="playlist">
 
-              <button className='playlistButton selected-playlist' id='playlist37i9dQZF1DXcBWIGoYBM5M' onClick={() => selectPlaylist('37i9dQZF1DXcBWIGoYBM5M')}>Today's Hits</button>
-              <button className='playlistButton' id='playlist37i9dQZF1DX4JAvHpjipBk' onClick={() => selectPlaylist('37i9dQZF1DX4JAvHpjipBk')}>New Music</button>
+
+            <div className="playlist">
+              <button className='playlistButton selected-playlist' id='playlist37i9dQZF1DX4JAvHpjipBk' onClick={() => selectPlaylist('37i9dQZF1DX4JAvHpjipBk')}>New Music</button>
+              {/* <button className='playlistButton' id='playlist37i9dQZF1DXcBWIGoYBM5M' onClick={() => selectPlaylist('37i9dQZF1DXcBWIGoYBM5M')}>Today's Hits</button>
               <button className='playlistButton' id='playlist37i9dQZF1DWVzMIQ8BnDGm' onClick={() => selectPlaylist('37i9dQZF1DWVzMIQ8BnDGm')}>Vibe</button>
-              <button className='playlistButton' id='playlist65hCGG0JopRfj06rs1ilmJ' onClick={() => selectPlaylist('65hCGG0JopRfj06rs1ilmJ')}>Critic's Picks</button>
+              <button className='playlistButton' id='playlist65hCGG0JopRfj06rs1ilmJ' onClick={() => selectPlaylist('65hCGG0JopRfj06rs1ilmJ')}>Critic's Picks</button> */}
 
             </div>
             {/* <h5 id="fetched">Fetched from Spotify</h5> */}
@@ -61,17 +62,23 @@ const NavbarPresentational = ({songs, muted, nav,  homepageControl, playlistCont
           </div>
 
           <div className="navbarContent-save droppable drophere"
-            onDragOver={dragOverEvent}
-            onDrop={dropEvent}>
-            <h2 id="content-title">Save Songs</h2>
-            <h4 id="content-title">Drag and drop blobs here to add songs to your personal collection.</h4>
-            <div className="collection">
-              <div id="miniPreview" className="miniPreviewScroll"></div>
-              <button id="collection-button" onClick={() => playlistAction()}>{playlistMessage}</button>
-            </div>
+          
+            // onDragOver={dragOverEvent}
+            // onDrop={dropEvent}>
+            // <h2 id="content-title">Save Songs</h2>
+            // <h4 id="content-title">Drag and drop blobs here to add songs to your personal collection.</h4>
+            // <div className="collection">
+            //   <div id="miniPreview" className="miniPreviewScroll"></div>
+            //   <button id="collection-button" onClick={() => playlistAction()}>{playlistMessage}</button>
+            // </div>
+
+            ><iframe title="vimeo-player" src="https://player.vimeo.com/video/503697607" width="340" height="160" frameborder="0" allowfullscreen></iframe>               
+
           </div>
 
+
         </div>
+
       </div>
   
       <div className={mutedStatus} onClick={() => handleClick(muted.audioMuted)}></div>
