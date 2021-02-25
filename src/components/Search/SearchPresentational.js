64a2d1b14e8f1c19d49b0979c25a2402f9d89879
Karolin Valaszkai <karolinv@kth.se>
2021-02-25 20:54:54 +0100

    Update SearchPresentational.js

--- src/components/Search/SearchPresentational.js
+++ src/components/Search/SearchPresentational.js
@@ -5,10 +5,9 @@ const SearchPresentational = ({ songs, whenDone, onResultsClick, onAdd, onLoadPl
   const [addCallback, addMessage] = onAdd;
 
   useEffect(() => {
-    onLoadPlaylist('37i9dQZF1DX4JAvHpjipBk')
+    onLoadPlaylist('4xTed278NxbQFt10i95vA9')
 }, []);
 
-// my-controlled-blobify: 4xTed278NxbQFt10i95vA9
 return (
     <div id="search" className="mainContent debug">
       <div id="resultsDiv" onClick={event => onResultsClick(event.target, songs)}></div>
