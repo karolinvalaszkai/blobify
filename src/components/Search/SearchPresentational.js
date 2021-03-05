da045a5c7783a83f7b35713e48f74bc749d72d93
Karolin Valaszkai <karolinv@kth.se>
2021-03-05 17:32:00 +0100

    update playlist

--- src/components/Search/SearchPresentational.js
+++ src/components/Search/SearchPresentational.js
@@ -5,9 +5,18 @@ const SearchPresentational = ({ songs, whenDone, onResultsClick, onAdd, onLoadPl
   const [addCallback, addMessage] = onAdd;
 
   useEffect(() => {
-    onLoadPlaylist('4xTed278NxbQFt10i95vA9')
+    onLoadPlaylist('0Sw0ccMCjtB7IWmTFYyGyB')
 }, []);
 
+//bäst just nu P3 - 65hCGG0JopRfj06rs1ilmJ
+// blobify test - 4xTed278NxbQFt10i95vA9
+// oyester - 37i9dQZF1DX2Wvd8VINtcF
+//g-major 0pfU5U7MSepfY3HUaq1TEb
+// slow 7naLna9BWXijv5DMuaPF8n
+// songs in a minor 7xYpodDUMKawIDpcIkTY0l
+//blobify 2 0Sw0ccMCjtB7IWmTFYyGyB
+//new music friday 37i9dQZF1DX4JAvHpjipBk
+//https://open.spotify.com/playlist/37i9dQZF1E4zxW4vWyzDQS?si=ab162e916e624a53
 return (
     <div id="search" className="mainContent debug">
       <div id="resultsDiv" onClick={event => onResultsClick(event.target, songs)}></div>
